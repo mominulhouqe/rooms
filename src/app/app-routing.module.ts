9e7ea28809fc77c65278fe3271806bfa72545040
mominul <ulhouqemomin39@gmail.com>
2023-07-22 14:44:15 +0600

    updated search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 // app-routing.module.ts
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import { AllRoomsComponent } from './all-rooms/all-rooms.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
+ 
   { path: '', component: RoomsComponent },
   { path: 'another-page', component: AllRoomsComponent },
+  { path: 'home', component: RoomsComponent },
+ 
 ];
 
 @NgModule({
